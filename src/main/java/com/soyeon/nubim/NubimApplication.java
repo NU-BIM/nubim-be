9c2157595bc9ea3ca6a482062cb83a15b5334f88
Seokhyun Lee <lseokhyun99@gmail.com>
2024-07-02 12:55:42 +0900

    project init

---
+++ src/main/java/com/soyeon/nubim/NubimApplication.java
@@ -0,0 +1,13 @@
+package com.soyeon.nubim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NubimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soyeon/nubim/NubimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soyeon.nubim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

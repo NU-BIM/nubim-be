a21768a953e9319ab7f5841391b0985fcfd3cf4a
sunwooiljoon <wnsdl1659@naver.com>
2024-07-09 16:59:34 +0900

    NB-24 : style - Album.java 코드 포맷
    - 탭 형식 맞지 않던거 수정
    - import 최적화

--- src/main/java/com/soyeon/nubim/domain/album/Album.java
+++ src/main/java/com/soyeon/nubim/domain/album/Album.java
@@ -1,21 +1,13 @@
 package com.soyeon.nubim.domain.album;
 
-import java.time.LocalDateTime;
-
 import com.soyeon.nubim.common.BaseEntity;
 import com.soyeon.nubim.domain.user.User;
-
 import com.vladmihalcea.hibernate.type.json.JsonType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.Type;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Setter
@@ -25,24 +17,24 @@ import org.hibernate.annotations.Type;
 @AllArgsConstructor
 public class Album extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long albumId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long albumId;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id", nullable = false)
-	private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
-	private String description;
+    private String description;
 
-	@Type(JsonType.class)
-	@Column(nullable = false, columnDefinition = "jsonb")
-	private String photoUrls;
+    @Type(JsonType.class)
+    @Column(nullable = false, columnDefinition = "jsonb")
+    private String photoUrls;
 
-	@Type(JsonType.class)
-	@Column(columnDefinition = "jsonb")
-	private String coordinate;
+    @Type(JsonType.class)
+    @Column(columnDefinition = "jsonb")
+    private String coordinate;
 
-	private LocalDateTime coordinateTime;
+    private LocalDateTime coordinateTime;
 
 }

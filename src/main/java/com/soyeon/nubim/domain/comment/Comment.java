57e7622ccd7f46042fce851635fd711741286120
sunwooiljoon <wnsdl1659@naver.com>
2024-07-09 11:29:15 +0900

    NB-40 : style, feat - Comment Entity 수정 - 코드 포맷 수정 - 댓글 길이 2200자로 설정 - Builder 패턴으로 변경

--- src/main/java/com/soyeon/nubim/domain/comment/Comment.java
+++ src/main/java/com/soyeon/nubim/domain/comment/Comment.java
@@ -3,38 +3,34 @@ package com.soyeon.nubim.domain.comment;
 import com.soyeon.nubim.common.BaseEntity;
 import com.soyeon.nubim.domain.post.Post;
 import com.soyeon.nubim.domain.user.User;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Data
-public class Comment extends BaseEntity{
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long commentId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id", nullable = false)
-	private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "post_id", nullable = false)
-	private Post post;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "parent_comment_id")
-	private Comment parentComment;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
 
-	@Column(nullable = false)
-	private String commentContent;
+    @Column(nullable = false, length = 2200)
+    private String commentContent;
 
 }

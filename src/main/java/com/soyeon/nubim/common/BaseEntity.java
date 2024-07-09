c60bddb8eadc0246368ef318ccc759fd5dd45107
aram-su <lseokhyun99@gmail.com>
2024-07-09 11:21:52 +0900

    NB-91 : feat - BaseEntity에 soft delete 기능 추가 및 Auditing 설정
    - @EntityListeners(AuditingEntityListener.class) 어노테이션 추가, JPA Auditing 활성화
    - @SQLDelete 어노테이션 추가, 소프트 삭제 구현
    - @SQLRestriction 어노테이션 추가, 삭제된 엔티티는 검색에서 제외
    - isDeleted 필드 추가 및 @JsonIgnore 처리

--- src/main/java/com/soyeon/nubim/common/BaseEntity.java
+++ src/main/java/com/soyeon/nubim/common/BaseEntity.java
@@ -3,13 +3,22 @@ package com.soyeon.nubim.common;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@SQLDelete(sql = "UPDATE #{entityName} SET is_deleted = true")
+@SQLRestriction("is_deleted = false")
 @Data
 public abstract class BaseEntity {
 
@@ -27,4 +36,7 @@ public abstract class BaseEntity {
 	@Column(nullable = false)
 	private LocalDateTime updatedAt;
 
+	@JsonIgnore
+	@Column(name = "is_deleted", nullable = false)
+	private Boolean isDeleted = Boolean.FALSE;
 }

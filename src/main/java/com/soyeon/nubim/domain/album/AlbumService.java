df7dd5dd0eec6a431a763387ee5addfbaf00c819
sunwooiljoon <wnsdl1659@naver.com>
2024-07-11 17:01:00 +0900

    NB-26 : fix - merge 과정에서 발생한 오류 수정
    - import 누락
    - 함수 누락

--- src/main/java/com/soyeon/nubim/domain/album/AlbumService.java
+++ src/main/java/com/soyeon/nubim/domain/album/AlbumService.java
@@ -1,10 +1,5 @@
 package com.soyeon.nubim.domain.album;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.soyeon.nubim.common.util.aws.S3PresignedUrlGenerator;
 import com.soyeon.nubim.domain.album.dto.AlbumCreateRequestDto;
 import com.soyeon.nubim.domain.album.dto.AlbumCreateResponseDto;
@@ -12,63 +7,71 @@ import com.soyeon.nubim.domain.album.dto.AlbumReadResponseDto;
 import com.soyeon.nubim.domain.album.mapper.AlbumMapper;
 import com.soyeon.nubim.domain.user.User;
 import com.soyeon.nubim.domain.user.UserService;
-
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AlbumService {
 
-	private final S3PresignedUrlGenerator s3PresignedUrlGenerator;
-	private final AlbumRepository albumRepository;
-	private final AlbumMapper albumMapper;
-	private final UserService userService;
+    private final S3PresignedUrlGenerator s3PresignedUrlGenerator;
+    private final AlbumRepository albumRepository;
+    private final AlbumMapper albumMapper;
+    private final UserService userService;
 
-	public AlbumCreateResponseDto createAlbum(AlbumCreateRequestDto albumCreateRequestDto) {
-		User user = userService.findById(albumCreateRequestDto.getUserId())
-			.orElseThrow(() -> new EntityNotFoundException("User not found, id: " + albumCreateRequestDto.getUserId()));
+    public Optional<Album> findById(Long id) {
+        return albumRepository.findById(id);
+    }
 
-		Album album = albumMapper.toEntity(albumCreateRequestDto, user);
-		List<Location> locations = album.getLocations();
-		for (Location location : locations) {
-			location.setAlbum(album);
-		}
+    public AlbumCreateResponseDto createAlbum(AlbumCreateRequestDto albumCreateRequestDto) {
+        User user = userService.findById(albumCreateRequestDto.getUserId())
+                .orElseThrow(() -> new EntityNotFoundException("User not found, id: " + albumCreateRequestDto.getUserId()));
 
-		Album savedAlbum = albumRepository.save(album);
+        Album album = albumMapper.toEntity(albumCreateRequestDto, user);
+        List<Location> locations = album.getLocations();
+        for (Location location : locations) {
+            location.setAlbum(album);
+        }
 
-		return albumMapper.toAlbumCreateResponseDto(savedAlbum);
-	}
+        Album savedAlbum = albumRepository.save(album);
 
-	public AlbumReadResponseDto findByIdWithLocations(Long albumId) {
-		Album album = albumRepository.findByIdWithLocations(albumId)
-			.orElseThrow(() -> new EntityNotFoundException("Album not found, albumId: " + albumId));
+        return albumMapper.toAlbumCreateResponseDto(savedAlbum);
+    }
 
-		return albumMapper.toAlbumReadResponseDto(album);
-	}
+    public AlbumReadResponseDto findByIdWithLocations(Long albumId) {
+        Album album = albumRepository.findByIdWithLocations(albumId)
+                .orElseThrow(() -> new EntityNotFoundException("Album not found, albumId: " + albumId));
 
-	public List<AlbumReadResponseDto> findAlbumsByUserId(Long userId) {
-		userService.findById(userId)
-			.orElseThrow(() -> new EntityNotFoundException("User not found, userId: " + userId));
+        return albumMapper.toAlbumReadResponseDto(album);
+    }
 
-		List<Album> albums = albumRepository.findByUserUserId(userId);
+    public List<AlbumReadResponseDto> findAlbumsByUserId(Long userId) {
+        userService.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User not found, userId: " + userId));
 
-		List<AlbumReadResponseDto> albumReadResponseDtos = new ArrayList<>(albums.size());
-		for (Album album : albums) {
-			albumReadResponseDtos.add(albumMapper.toAlbumReadResponseDto(album));
-		}
-		return albumReadResponseDtos;
-	}
+        List<Album> albums = albumRepository.findByUserUserId(userId);
 
-	public void deleteAlbum(Long albumId) {
-		albumRepository.findByIdWithLocations(albumId)
-			.orElseThrow(() -> new EntityNotFoundException("Album not found with albumId: " + albumId));
+        List<AlbumReadResponseDto> albumReadResponseDtos = new ArrayList<>(albums.size());
+        for (Album album : albums) {
+            albumReadResponseDtos.add(albumMapper.toAlbumReadResponseDto(album));
+        }
+        return albumReadResponseDtos;
+    }
 
-		albumRepository.deleteById(albumId);
-	}
+    public void deleteAlbum(Long albumId) {
+        albumRepository.findByIdWithLocations(albumId)
+                .orElseThrow(() -> new EntityNotFoundException("Album not found with albumId: " + albumId));
 
-	public List<String> handlePhotoUploadUrlsGeneration(List<String> contentTypes) {
-		return s3PresignedUrlGenerator.generatePresignedUrls(contentTypes);
-	}
+        albumRepository.deleteById(albumId);
+    }
+
+    public List<String> handlePhotoUploadUrlsGeneration(List<String> contentTypes) {
+        return s3PresignedUrlGenerator.generatePresignedUrls(contentTypes);
+    }
 
 }

6643ebde8fcf5abfea7a700dcd62f46bed653a5e
aram-su <lseokhyun99@gmail.com>
2024-07-23 22:29:41 +0900

    NB-140 : feat - Google OAuth 로그인 후 JWT 토큰 발급 기능 구현
    - GoogleOAuthLoginService 클래스 기능 추가
     - Jwt Access Token 생성 및 응답 헤더에 추가
     - Refresh Token 생성 및 응답 쿠키에 추가
     - Refresh Token Entity 생성 및 DB 저장 로직 구현
    
    - createAccessTokenHeader()
     - Access Token 생성 및 헤더 설정
    
    - createRefreshTokenCookie()
     - Refresh Token 생성 및 쿠키 설정
    
    - upsertRefreshTokenEntity()
     - Refresh Token Entity 생성 또는 업데이트 및 DB 저장

--- src/main/java/com/soyeon/nubim/security/oauth/GoogleOAuthLoginService.java
+++ src/main/java/com/soyeon/nubim/security/oauth/GoogleOAuthLoginService.java
@@ -1,8 +1,11 @@
 package com.soyeon.nubim.security.oauth;
 
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +13,11 @@ import org.springframework.web.client.RestTemplate;
 import com.soyeon.nubim.domain.user.User;
 import com.soyeon.nubim.domain.user.UserNotFoundException;
 import com.soyeon.nubim.domain.user.UserService;
+import com.soyeon.nubim.security.jwt.JwtTokenProvider;
+import com.soyeon.nubim.security.refreshtoken.RefreshToken;
+import com.soyeon.nubim.security.refreshtoken.RefreshTokenService;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -18,10 +25,14 @@ import lombok.RequiredArgsConstructor;
 public class GoogleOAuthLoginService {
 
 	private static final String USER_INFO_ENDPOINT = "https://www.googleapis.com/oauth2/v3/userinfo";
+	public static final int REFRESH_TOKEN_MAX_AGE_SECONDS = 604800; //60 * 60 * 24 * 7
 	private final RestTemplate restTemplate = new RestTemplate();
 
 	private final UserService userService;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final RefreshTokenService refreshTokenService;
 
+	// TODO : redirect 기능 추가 예정
 	public ResponseEntity<String> authenticateWithGoogleToken(String oauthAccessToken) {
 		validateOAuthAccessToken(oauthAccessToken);
 
@@ -29,7 +40,12 @@ public class GoogleOAuthLoginService {
 
 		User user = upsertUserFromGoogleUserInfo(userInfo);
 
+		HttpHeaders headers = createAccessTokenHeader(user);
+		ResponseCookie refreshTokenCookie = createRefreshTokenCookie(user);
+
 		return ResponseEntity.ok()
+			.headers(headers)
+			.header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
 			.body("success");
 	}
 
@@ -68,6 +84,40 @@ public class GoogleOAuthLoginService {
 			user = userInfo.toUserEntity();
 		}
 		return userService.saveUser(user);
+	}
+
+	private HttpHeaders createAccessTokenHeader(User user) {
+		String accessToken = jwtTokenProvider.generateAccessToken(user.getEmail(), user.getRole().name());
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Bearer " + accessToken);
+		return headers;
+	}
+
+	private ResponseCookie createRefreshTokenCookie(User user) {
+		String refreshToken = jwtTokenProvider.generateRefreshToken(user.getEmail());
+
+		upsertRefreshTokenEntity(refreshToken, user.getEmail());
+
+		return ResponseCookie.from("refresh_token", refreshToken)
+			.httpOnly(true)
+			.secure(true)
+			.path("/")
+			.maxAge(REFRESH_TOKEN_MAX_AGE_SECONDS)
+			.sameSite("Strict")
+			.build();
+	}
+
+	private void upsertRefreshTokenEntity(String refreshToken, String userEmail) {
+		RefreshToken refreshTokenEntity;
+		LocalDateTime expiresAt = LocalDateTime.now().plusSeconds(REFRESH_TOKEN_MAX_AGE_SECONDS);
+		try {
+			refreshTokenEntity = refreshTokenService.findByEmail(userEmail);
+			refreshTokenEntity.updateToken(refreshToken, expiresAt);
+		} catch (EntityNotFoundException e) {
+
+			refreshTokenEntity = new RefreshToken(refreshToken, userEmail, expiresAt);
+		}
+		refreshTokenService.saveRefreshToken(refreshTokenEntity);
 	}
 
 }

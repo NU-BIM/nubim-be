4a711ad2ff481b4f37f7ba6102e7e57a76999d3c
aram-su <lseokhyun99@gmail.com>
2024-07-29 17:23:24 +0900

    NB-154 : refactor - Refresh Token 요청 형식 변경
    1. TokenRenewalRequestDto 구현
     - Access Token 재발급을 위해 Refresh Token을 Request Body에 담아 전달할 DTO 클래스 구현
    
    2. 컨트롤러 메소드 파라미터 변경
     - 변경 전 : @CookieValue("refresh_token") String refreshToken
     - 변경 후 : @RequestBody TokenRenewalRequestDto tokenRenewalRequestDto

--- src/main/java/com/soyeon/nubim/security/refreshtoken/RefreshTokenControllerV1.java
+++ src/main/java/com/soyeon/nubim/security/refreshtoken/RefreshTokenControllerV1.java
@@ -1,12 +1,13 @@
 package com.soyeon.nubim.security.refreshtoken;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.soyeon.nubim.security.jwt.JwtTokenResponseDto;
+import com.soyeon.nubim.security.jwt.dto.TokenRenewalRequestDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,7 +19,8 @@ public class RefreshTokenControllerV1 {
 	private final RefreshTokenService refreshTokenService;
 
 	@PostMapping("/new-access-token")
-	public ResponseEntity<JwtTokenResponseDto> getNewAccessToken(@CookieValue("refresh_token") String refreshToken) {
-		return refreshTokenService.renewAccessToken(refreshToken);
+	public ResponseEntity<JwtTokenResponseDto> getNewAccessToken(
+		@RequestBody TokenRenewalRequestDto tokenRenewalRequestDto) {
+		return refreshTokenService.renewAccessToken(tokenRenewalRequestDto.getRefreshToken());
 	}
 }

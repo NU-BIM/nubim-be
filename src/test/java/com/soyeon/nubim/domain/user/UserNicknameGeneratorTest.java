277783728c6ac2f4c41721a71bd55687f7103648
sunwooiljoon <wnsdl1659@naver.com>
2024-08-01 15:54:21 +0900

    NB-168 : test - 자동 생성 닉네임 검증 테스트 작성
    - 자동 생성된 닉네임이 닉네임 규정에 준수하는지 검증하는 유닛 테스트 작성

---
+++ src/test/java/com/soyeon/nubim/domain/user/UserNicknameGeneratorTest.java
@@ -0,0 +1,23 @@
+package com.soyeon.nubim.domain.user;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserNicknameGeneratorTest {
+
+	@Test
+	void generateTest() {
+		for (int i = 0; i < 10000; i++) {
+			// given
+			String generatedNickname = UserNicknameGenerator.generate();
+
+			//when
+
+			// then
+			assertThat(generatedNickname)
+				.hasSizeBetween(User.NicknamePolicy.MIN_LENGTH, User.NicknamePolicy.MAX_LENGTH)
+				.matches(User.NicknamePolicy.REGEXP);
+		}
+	}
+}

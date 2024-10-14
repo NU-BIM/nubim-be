863df4bc682838cec4fe375642c4bbd9713728a5
sunwooiljoon <wnsdl1659@naver.com>
2024-10-14 15:46:31 +0900

    NB-234 : feat - search-nickname api 수정
    - 본인 정보 검색 시, isFollowed null 로 반환

--- src/main/java/com/soyeon/nubim/domain/search/SearchService.java
+++ src/main/java/com/soyeon/nubim/domain/search/SearchService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.soyeon.nubim.domain.post.PostService;
 import com.soyeon.nubim.domain.post.dto.PostMainResponseDto;
+import com.soyeon.nubim.domain.user.LoggedInUserService;
 import com.soyeon.nubim.domain.user.User;
 import com.soyeon.nubim.domain.user.UserMapper;
 import com.soyeon.nubim.domain.user.UserService;
@@ -19,10 +20,18 @@ public class SearchService {
 	private final UserService userService;
 	private final PostService postService;
 	private final UserMapper userMapper;
+	private final LoggedInUserService loggedInUserService;
 
 	public Page<UserSimpleWithIsFollowedResponseDto> searchUsers(String query, Pageable pageable) {
 		Page<User> users = userService.searchUserByNickname(pageable, query);
-		return users.map(userMapper::toUserSimpleWithIsFollowedResponseDto);
+		return users.map(user -> {
+			UserSimpleWithIsFollowedResponseDto userResponseDto = userMapper.toUserSimpleWithIsFollowedResponseDto(
+				user);
+			if (user.getUserId() == loggedInUserService.getCurrentUser().getUserId()) {
+				userResponseDto.setIsFollowed(null);
+			}
+			return userResponseDto;
+		});
 	}
 
 	public Page<PostMainResponseDto> searchPosts(String query, Pageable pageable) {

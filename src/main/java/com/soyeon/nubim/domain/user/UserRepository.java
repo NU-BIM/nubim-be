bbe5332c9a6b58842c7871bc0adceb2ce03819d6
aram-su <lseokhyun99@gmail.com>
2024-10-18 11:21:29 +0900

    NB-241 : refactor - 삭제 유저 조회 쿼리 수정
    1. UserRepository
     - existsByEmailAndIsDeletedTrue(String email) 메소드 쿼리 수정
      - case문에서 exists로 수정하여 더 효율적인 쿼리로 수정함

--- src/main/java/com/soyeon/nubim/domain/user/UserRepository.java
+++ src/main/java/com/soyeon/nubim/domain/user/UserRepository.java
@@ -24,7 +24,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	Boolean existsByNickname(String nickname);
 
-	@Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN true ELSE false END FROM users WHERE email = :email", nativeQuery = true)
+	@Query(value = "SELECT EXISTS(SELECT 1 FROM users WHERE email = :email AND is_deleted = true)", nativeQuery = true)
 	Boolean existsByEmailAndIsDeletedTrue(String email);
 
 	Page<User> findByNicknameStartsWith(Pageable pageable, String query);
